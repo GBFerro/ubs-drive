8f8c83fe1d5f1e7b2222aa2ee4e363be373551aa
Giovani Ferro <101731657+GBFerro@users.noreply.github.com>
2024-09-10 22:36:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 # Configurações do AWS S3
-AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
-AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
-S3_BUCKET = os.getenv('S3_BUCKET')
+AWS_ACCESS_KEY = os.environ('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.environ('AWS_SECRET_KEY')
+S3_BUCKET = os.environ('S3_BUCKET')
 
 # Configurações do MongoDB
-MONGO_URI = os.getenv('MONGO_URI')  # URL da conexão do MongoDB
+MONGO_URI = os.environ('MONGO_URI')  # URL da conexão do MongoDB
 DATABASE_NAME = 'ubs_files'
 COLLECTION_NAME = 'files_metadata'
